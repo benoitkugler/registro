20c1de0ff2cb66947656ddcd0a273ebe47053f66
Benoit KUGLER <benoit.kugler@gmail.com>
2025-01-22 11:44:47 +0100

    refactor config

--- server/main.go
+++ server/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"registro/config"
+)
+
+func main() {
+	config, err := config.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = config
+}
