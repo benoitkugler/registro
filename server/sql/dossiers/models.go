95ceb287170fc131b7719b9d2f6296a42bf9e009
Benoit KUGLER <benoit.kugler@gmail.com>
2025-02-10 13:19:08 +0100

    [dossiers] add Paiment description

--- server/sql/dossiers/models.go
+++ server/sql/dossiers/models.go
@@ -68,7 +68,8 @@ type Paiement struct {
 	Id        IdPaiement
 	IdDossier IdDossier `gomacro-sql-on-delete:"CASCADE"`
 
-	IsAcompte       bool
+
+	IsAcompte       bool  
 	IsRemboursement bool
 
 	Montant Montant
@@ -79,4 +80,22 @@ type Paiement struct {
 	Label string
 	// Details peut stocker un motif ou la date d'encaissement d'un chèque
 	Details string
+}
+
+// DescriptionHTML renvoie une description et le montant, au format HTML
+func (r Paiement) DescriptionHTML(taux Taux) (string, string) {
+	var payeur string
+	if r.IsAcompte {
+		payeur = fmt.Sprintf("Acompte de <i>%s</i> au %s", r.Payeur, r.Date)
+	} else if r.IsRemboursement {
+		payeur = fmt.Sprintf("Remboursement au %s", r.Date))
+	} else {
+		payeur = fmt.Sprintf("Paiement de <i>%s</i> au %s", r.Payeur, r.Date)
+	}
+	m := r.Montant
+	if r.IsRemboursement {
+		m.Cent *= -1
+	}
+	montant := fmt.Sprintf("<i>%s</i>", taux.Convert(r.Montant).String())
+	return payeur, montant
 }
