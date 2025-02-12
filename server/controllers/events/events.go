a36041878aba14f41cda61c3a4010fa0bb4e54e6
Benoit KUGLER <benoit.kugler@gmail.com>
2025-02-12 14:22:43 +0100

    WIP : initial public events

--- server/controllers/events/events.go
+++ server/controllers/events/events.go
@@ -1,1 +1,23 @@
 package events
+
+type Event struct {
+	Created time.Time 
+	Content EventContent
+}
+
+// EventContent exposes on event on the dossier track: either a dossier.Paiement or an event.Event
+type EventContent interface{
+	isEventContent()
+}
+
+type Supprime struct{}
+type Inscription struct{}
+type AccuseReception struct{}
+type Message struct{}
+type Facture  struct{}      
+type CampDocs   struct{}     
+type Attestation   struct{} 
+type PlaceLiberee struct{}
+type Sondage    struct{}
+type Paiement struct{}
+
