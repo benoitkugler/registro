2b420f30ecf2fa588400dd52371d5f1a8ac0d361
Benoit KUGLER <benoit.kugler@gmail.com>
2025-03-25 14:10:11 +0100

    [server] add build script

---
+++ server/build_deploy.sh
@@ -0,0 +1,11 @@
+echo "Pulling git..." &&
+git pull &&
+echo "Entering server/" &&
+cd src && 
+echo "Building (downloading deps if needed)..." && 
+go build *.go &&
+echo "Done." && 
+echo "Moving executable and leaving source..."
+cd .. && 
+mv server/main . && 
+echo "Done"
