2b0accc5ad9b48b49fba6aba402503af4a3b85c1
Benoit KUGLER <benoit.kugler@gmail.com>
2025-10-02 15:10:30 +0200

    [inscription] refactor participant start, closes #110

--- registro-web/src/clients/inscription/components/ParticipantRow.vue
+++ registro-web/src/clients/inscription/components/ParticipantRow.vue
@@ -82,12 +82,12 @@
       </v-row>
     </v-card-text>
     <v-card-actions>
+      <v-spacer></v-spacer>
       <v-btn color="red" @click="emit('delete')" variant="outlined">
         <template #prepend>
           <v-icon>mdi-delete</v-icon>
           Enlever ce participant
         </template>
-        <v-spacer></v-spacer>
       </v-btn>
     </v-card-actions>
   </v-card>
