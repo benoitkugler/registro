0c8ebb94be20510cef73aadf76d29a5f902297df
Benoit KUGLER <benoit.kugler@gmail.com>
2026-09-13 17:56:40 +0200

    [backoffice, directeurs] better search when selecting camps, closes #263

--- registro-web/src/components/SelectCamp.vue
+++ registro-web/src/components/SelectCamp.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-select
+  <v-autocomplete
     :density="props.defaultStyle ? undefined : 'compact'"
     :variant="props.defaultStyle ? undefined : 'outlined'"
     :label="props.label"
@@ -10,34 +10,14 @@
     @update:model-value="(id) => (modelValue = nullableToZeroable(id))"
     no-data-text="Aucun séjour n'existe."
     hide-details
-    autocomplete="off"
   >
-    <template #prepend>
-      <v-tooltip
-        :text="
-          showTerminatedCamps
-            ? 'Masquer les séjours terminés'
-            : 'Afficher les séjours terminés'
-        "
-      >
-        <template #activator="{ props: tooltipProps }">
-          <v-btn
-            size="small"
-            :variant="showTerminatedCamps ? 'tonal' : 'flat'"
-            v-bind="tooltipProps"
-            icon="mdi-history"
-            @click="showTerminatedCamps = !showTerminatedCamps"
-          ></v-btn>
-        </template>
-      </v-tooltip>
-    </template>
-  </v-select>
+  </v-autocomplete>
 </template>
 
 <script setup lang="ts">
 import type { CampItem, IdCamp } from "@/clients/backoffice/logic/api";
 import { Camps, nullableToZeroable, zeroableToNullable } from "@/utils";
-import { computed, ref, watch } from "vue";
+import { computed, ref } from "vue";
 const props = defineProps<{
   label: string;
   camps: CampItem[];
@@ -47,12 +27,22 @@ const props = defineProps<{
 
 const modelValue = defineModel<IdCamp>({ required: true });
 
-const showTerminatedCamps = ref(false);
-const campItems = computed(() =>
-  props.camps
-    .filter((c) => (showTerminatedCamps.value ? true : !c.IsOld))
-    .map((c) => ({ title: Camps.label(c), value: c.Id })),
-);
+const campItems = computed(() => {
+  const newCamps: (
+    | { type: "divider" }
+    | { type: "subheader"; title: string }
+    | { value: IdCamp; title: string }
+  )[] = props.camps
+    .filter((c) => !c.IsOld)
+    .map((c) => ({ title: Camps.label(c), value: c.Id }));
+  const oldCamps = props.camps
+    .filter((c) => c.IsOld)
+    .map((c) => ({ title: Camps.label(c), value: c.Id }));
+  return newCamps.concat(
+    [{ type: "divider" }, { type: "subheader", title: "Séjours terminés" }],
+    oldCamps,
+  );
+});
 </script>
 
 <style scoped></style>
